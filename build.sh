5b926d4662dbac4ef986a45ec2647486f721817d
Whitey <matt.mccarragher@online-rewards.com>
2017-07-12 15:55:45 -0400

    fucking christ

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+mkdir -p buildclient
+wget -nc http://zandronum.com/essentials/fmod/fmodapi42416linux64.tar.gz 
+tar -xvzf fmodapi42416linux64.tar.gz
 cd buildclient
 cmake -DCMAKE_BUILD_TYPE=Release -DFMOD_LIBRARY=`pwd`/../fmodapi42416linux64/api/lib/libfmodex64-4.24.16.so -DFMOD_INCLUDE_DIR=`pwd`/../fmodapi42416linux64/api/inc .. 
 make
